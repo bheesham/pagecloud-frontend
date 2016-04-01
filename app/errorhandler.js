747311453111b842965a1841d176d16327e20e5b
Guy Hughes <g@gxg.me>
2016-04-01 18:58:39 -0400

    errorhandler

---
+++ app/errorhandler.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+
+/**
+ * // call me maybe, like so:
+ * if(errorhandler(node_id,json)){
+ *  return;
+ * }
+ */
+
+export default function (node_id,json) {
+
+  // const response = JSON.parse(fs.readFileSync()); 
+  if(Object.prototype.hasOwnProperty.call(json,"errors")){
+   let node = document.getElementById(node_id); 
+   $(node).appendChild("1");
+   return true;
+  }
+  return false;
+
+}
+
