9939615b231200d62820af25ecfd7a9a5d643f53
Guy Hughes <g@gxg.me>
2016-04-01 13:06:00 -0400

    es2015 is here

--- index.js
+++ index.js
@@ -15,4 +15,34 @@ import browserify from 'browserify-middleware'
 const port = 9000;
 
 
+export default app = express()
+log.add(log.transports.File, {filename: 'index.log'})
+app.use(morgan('dev'))
+.use(cookieparser())
+.use(session({
+  secret: 'aamazingsecretthatkeepseverythingsafe',
+  resave: false
+}))
+.set('view engine','jade')
+.set('views',__dirname+'/views')
+.use(sass({
+   src: __dirname + '/sass',
+   dest: __dirname + '/static',
+   response: true,
+   debug: false,
+   outputStyle: 'compressed',
+   sourceComments: false,
+   sourceMap: 'static/sourcemap',
+   sourceMapEmbed: true,
+   error: () => {
+      console.log("error with sass middleware")
+   }
+}))
+
+export const server = http.createServer(apps).listen(port);
+server.on('listening',()=> { "server is listening on "+port+" ☺"; });
+process.on('SIGINT',()=>{
+  console.log("SIGINT received.")
+  process.exit(0)
+})
 
