81ac76ad5b14d75b81594a7084818a0758d5c5f3
Himesh Patel <himeshapatel@gmail.com>
2016-04-02 11:12:03 -0400

    added referrer plot

--- app/referrers.js
+++ app/referrers.js
@@ -1,3 +1,34 @@
-export default function () {
+import errorHandler from './errorhandler'
+export default function (node_Id) {
+  $.getJSON('/tests/referrers.json',json => {
+    if(errorHandler(node_Id, json)){
+      return;
+    }
 
+    var referrers = [];
+    var count = [];
+
+    for (var i = 0; i < json.data.referrers.length; i++) {
+      referrers[i] = json.data.referrers[i].name;
+      count[i] = json.data.referrers[i].count;
+    }
+
+    count.sort(function(a, b){return a-b});
+
+    console.log(count);
+
+    var data = [{
+      type: 'bar',
+      x: count,
+      y: referrers,
+      orientation: 'h'
+    }];
+
+    // var layout = {
+    //   height: 400,
+    //   width: 500
+    // };
+
+    Plotly.newPlot(node_Id, data);
+  });
 }
