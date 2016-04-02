1742a258b1f226e371eafdafb4689a45ecd1331c
Guy Hughes <g@gxg.me>
2016-04-02 14:09:08 -0400

    referrers play - not done

--- app/referrers.js
+++ app/referrers.js
@@ -1,36 +1,47 @@
 import errorHandler from './errorhandler'
 import sortByKey from './sortByKey'
 export default function(node_Id) {
-    $.getJSON('/api/referrers', json => {
-        if (errorHandler(node_Id, json)) {
-            return;
-        }
+  "use strict";
+  $.getJSON('/api/referrers', json => {
+    if (errorHandler(node_Id, json)) {
+      return;
+    }
 
-        var referrers = json.data.referrers;
-        var name = [];
-        var count = [];
+    let referrers = sortByKey(json.data.referrers,'count');
+    let referrers_showable = [];
+    let name = [];
+    let count = [];
 
-        console.log(referrers);
-        var referrers = sortByKey(referrers, 'count');
-        console.log(referrers);
+    for (let i = 0; i < referrers.length && referrers_showable.length < 15; i++) {
+      if(referrers[i].count <= 2){
+        continue;
+      }
+      if(referrers[i].name==="pagecloud.com"){
+        continue;
+      }
+      if(referrers[i].name===""){
+        continue;
+      }
+      referrers_showable.push(referrers[i]);
+    }
 
-        for (var i = 0; i < referrers.length; i++) {
-            name[i] = referrers[i].name;
-            count[i] = referrers[i].count;
-        }
+    for (let i = 0; i < referrers_showable.length; i++) {
+      name[i] = referrers_showable[i].name;
+      count[i] = referrers_showable[i].count;
+    }
 
-        var data = [{
-            type: 'bar',
-            x: count,
-            y: name,
-            orientation: 'h'
-        }];
+    var data = [{
+      type: 'bar',
+      x: count,
+      y: name,
+      orientation: 'h'
+    }];
 
-        // var layout = {
-        //   height: 400,
-        //   width: 500
-        // };
+    // var layout = {
+    //   height: 400,
+    //   width: 500
+    // };
 
-        Plotly.newPlot(node_Id, data);
-    });
+    Plotly.newPlot(node_Id, data);
+  });
 }
